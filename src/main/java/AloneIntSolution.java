2c99cd122c0f9685c366858296e7ebc79d6c8055
Aquilla Silva Leite <aquilla11@hotmail.com>
2021-02-19 21:00:35 -0300

    challenges solutions

--- src/main/java/AloneIntSolution.java
+++ src/main/java/AloneIntSolution.java
@@ -1,6 +1,18 @@
+import java.util.Arrays;
+import java.util.function.Predicate;
+
 public class AloneIntSolution {
 
     public int execute(int[] array) {
-        return 0;
+        Predicate<Integer> uniqueNumberCount = value -> {
+            long occurrences = Arrays.stream(array)
+                    .filter(value::equals)
+                    .count();
+            return occurrences == 1;
+        };
+
+        return Arrays.stream(array)
+                .filter(uniqueNumberCount::test)
+                .sum();
     }
 }
--- src/main/java/DiagonalDifferenceSolution.java
+++ src/main/java/DiagonalDifferenceSolution.java
@@ -2,6 +2,12 @@ import java.util.List;
 
 public class DiagonalDifferenceSolution {
     public int execute(List<List<Integer>> args) {
-        return 0;
+        int n = args.size(), a = 0, b = 0;
+
+        for (int x = 0; x < n; x++) {
+            a += args.get(x).get(x);
+            b += args.get(x).get((n - 1) - x);
+        }
+        return Math.abs(a - b);
     }
 }
