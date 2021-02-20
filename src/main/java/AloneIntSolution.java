38f07929d28b0020645277781859a7fcd3b65686
Igor Meira <igormeira.dev@gmail.com>
2021-02-20 09:49:50 -0300

    Solução dos desafios

--- src/main/java/AloneIntSolution.java
+++ src/main/java/AloneIntSolution.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AloneIntSolution {
 
+    /* Explicação
+    * 1- Pegar o primeiro elemento do array.
+    * 2- Verificar se o número atual já está na lista de numeros errados.
+    *    Caso esteja, pega o próximo número.
+    * 3- Comparar o número atual com os números à frente dele.
+    *    Se existir algum número igual, ele é adicionado na lista de números errados.
+    *    Caso contrário, é retornado como resposta.
+    * 4- Repetir o processo. Caso chegue no último número da lista, ele é retornado. */
+
     public int execute(int[] array) {
-        return 0;
+        int currentNumber;
+        List<Integer> wrongNumber = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            currentNumber = array[i];
+            if (!wrongNumber.contains(currentNumber)) {
+                if (i == array.length - 1) return currentNumber;
+
+                for (int j = i+1; j < array.length; j++) {
+                    if (currentNumber == array[j]) {
+                        wrongNumber.add(currentNumber);
+                        break;
+                    }
+                }
+
+                if (!wrongNumber.contains(currentNumber)) return currentNumber;
+            }
+        }
+
+        return -1;
     }
 }
--- src/main/java/BalancePointSolution.java
+++ src/main/java/BalancePointSolution.java
@@ -1,7 +1,60 @@
+import java.util.Arrays;
+
 public class BalancePointSolution {
 
+    /* Explicação
+     * 1- Estabelecer um pivô, um próximo index para a esquerda e um para a direita.
+     * 2- Estabelecer um subarray, do início do array original até o pivot.
+     * 3- Estabelecer um subarray, do pivô até final do array original.
+     * 3- Realizar a soma dos elementos em ambos os subarrays.
+     * 4- Caso as somas sejam iguais, retorna o pivô.
+     *    Caso contrário, é feita uma comparação para saber quem será o próximo index.
+     *    Caso o pivô seja o index do meio ou maior que o do meio do array original,
+     *    chamamos novamente a função, passando o próximo index da esquerda como pivô e
+     *    incrementando o valor do próximo index da esquerda.
+     *    Caso o pivô seja menor que o do meio do array original,
+     *    chamamos novamente a função, passando o próximo index da direita como pivô e
+     *    incrementando o valor do próximo index da direita.
+     * 5- Como a ordem de movimento é meio-esquerda-direita, caso o index da direita chegue
+     *    ao valor igual ao tamanho do array original, retornamos -1.
+     *    Indicando que não existe uma solução.  */
+
     public int execute(int[] args) {
-        return 0;
+
+        int size = args.length;
+        int pivot = (size - 1) / 2;
+        int nextRightIndex = pivot + 1;
+        int nextLeftIndex = pivot - 1;
+
+        return findBalance(args, pivot, nextLeftIndex, nextRightIndex);
+    }
+
+    private int findBalance(int[] args, int pivot, int nextLeft, int nextRight) {
+        int middle = (args.length - 1) / 2;
+
+        int startLeftArray = 0;
+        int endLeftArray = pivot;
+        int[] leftSubarray = Arrays.copyOfRange(args, startLeftArray, endLeftArray);
+
+        int startRightArray = pivot + 1;
+        int endRightArray = args.length;
+        int[] rightSubarray = Arrays.copyOfRange(args, startRightArray, endRightArray);
+
+        int sumLeft = Arrays.stream(leftSubarray).sum();
+        int sumRight = Arrays.stream(rightSubarray).sum();
+
+        if (sumLeft == sumRight) return pivot;
+        else {
+            if (pivot >= middle) {
+                int moveLeft = Math.max((nextLeft - 1), 0);
+                return findBalance(args, nextLeft, moveLeft, nextRight);
+            }
+            else { // pivot < middle
+                int moveRight = nextRight + 1;
+                if (moveRight == args.length) return - 1;
+                else return findBalance(args, nextRight, nextLeft, moveRight);
+            }
+        }
     }
 
 }
