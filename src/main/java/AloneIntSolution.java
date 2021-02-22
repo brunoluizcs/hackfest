54d7d8dc86a248e6876186f820721aae58d8a35c
Sergio Guimaraes <sergiofilho88@gmail.com>
2021-02-22 11:20:41 -0300

    Everis HackFest! - Participante: Sergio Guimaraes Costa Filho

--- src/main/java/AloneIntSolution.java
+++ src/main/java/AloneIntSolution.java
@@ -1,6 +1,21 @@
 public class AloneIntSolution {
 
     public int execute(int[] array) {
+
+        boolean numeroRepetido = false;
+
+        for (int i = 0; i <= array.length - 1 ; i++) {
+            for (int k = 0; k <= array.length - 1 ; k++) {
+                if (i != k)
+                    if (array[i] == array[k])
+                        numeroRepetido = true;
+            }
+            if (numeroRepetido == false) {
+                return array[i];
+            }
+            numeroRepetido = false;
+        }
+
         return 0;
     }
 }
